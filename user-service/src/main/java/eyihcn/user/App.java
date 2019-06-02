f81e28888a55ad5572394dfda0313e19feed59c6
eyihcn <526048537@qq.com>
2019-06-02 23:45:40 +0800

    eyihcn

--- user-service/src/main/java/eyihcn/user/App.java
+++ user-service/src/main/java/eyihcn/user/App.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +18,22 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+
+	/**
+	 * 分页插件
+	 */
+	@Bean
+	public PaginationInterceptor paginationInterceptor() {
+		return new PaginationInterceptor();
+	}
+
+	/**
+	 * SQL执行效率插件
+	 */
+	@Bean
+	@Profile({ "dev", "test" }) // 设置 dev test 环境开启
+	public PerformanceInterceptor performanceInterceptor() {
+		return new PerformanceInterceptor();
 	}
 }
